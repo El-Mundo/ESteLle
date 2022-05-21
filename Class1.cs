cba974a9ed7099d52e07fa78e54816b15767dce7
El-Mundo <superbluecat71@yahoo.com>
2022-05-21 20:21:14 +0800

    First upload

---
+++ Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AlibabaCloud.SDK.VIAPI.Utils;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Class1
+{
+	public Class1()
+	{
+        static void Main(string[] args)
+        {
+            string accessKeyId = "accessKeyId";   // 您的AccessKeyID
+            string accessSecret = "accessKeySecret";   // 您的AccessKeySecret
+            string imageUrl = "https://tse1-mm.cn.bing.net/th/id/R-C.c82590d4c1f8e1a8b8ffeb5c7674715f?rik=plgAnz%2bkRhPqnw&riu=http%3a%2f%2fdoctormacro.com%2fImages%2fBergman%2c+Ingrid%2fAnnex%2fAnnex+-+Bergman%2c+Ingrid+(Indiscreet)_01.jpg&ehk=G4XQuJisjx734aoE4B4zIn%2fqvwxePLIcpCSSgnwzw7s%3d&risl=&pid=ImgRaw&r=0";  // 上传成功后，返回上传后的文件地址
+            FileUtils fileobj = FileUtils.getInstance(accessKeyId, accessSecret);
+            string result = fileobj.Upload(imageUrl);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using AlibabaCloud.SDK.VIAPI.Utils;
+using Microsoft.Win32;
+
+namespace EsteLle
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void GenerateURL()
+        {
+            string accessKeyId = "LTAI5t9Bv7zv7WsvuCXvbYac";   // 您的AccessKeyID
+            string accessSecret = "euPYCDVgMSQwMTZgUnoYDaqDCJwKUQ";   // 您的AccessKeySecret
+            string imageUrl = PathPanel.Text;  // 上传成功后，返回上传后的文件地址
+            string result = "";
+            try 
+            { 
+                FileUtils fileobj = FileUtils.getInstance(accessKeyId, accessSecret);
+                result = fileobj.Upload(imageUrl);
+            }
+            catch(Exception e)
+            {
+                ResultPanel.Text = e.Message;
+            }
+            Console.WriteLine(result);
+            ResultPanel.Text = result;
+        }
+
+        private void SelectFile()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try 
+            { 
+                ImagePanel.Source = new BitmapImage(new Uri(PathPanel.Text));
+            }
+            catch(Exception ex)
+            {
+                ResultPanel.Text = "Cannot open image, " + ex.Message;
+            }
+            GenerateURL();
+        }
+    }
+}
